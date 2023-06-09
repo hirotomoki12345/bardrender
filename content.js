076ff3e3f85e33467cda04a04114fc145e181493
hirotomoki12345 <127632545+hirotomoki12345@users.noreply.github.com>
2023-06-09 11:11:36 +0900

    Add files via upload

--- content.js
+++ content.js
@@ -1,14 +1,13 @@
-// 要素を非表示にする関数
-function hideElement(selector) {
-  var elements = document.querySelectorAll(selector);
-  elements.forEach(function(element) {
-    element.style.display = 'none';
-  });
+function hideAds() {
+  var adClasses = ['.adsbygoogle', '.css-1dbjc4n r-1awozwy r-18u37iz r-16y2uox r-1wtj0ep r-mk0yit', '.ad-banner', '.cat032Wrap', '.m2a8e1__content', '.m2a8e1__module', '.c-sub-content-block', '.mab9d4__anchor', '.pcBanner spNone', '#mailmagazine_banner']; // 非表示にしたい広告要素のクラス名リスト
+
+  // 各クラス名に対して広告要素を非表示にする
+  for (var i = 0; i < adClasses.length; i++) {
+    var ads = document.querySelectorAll(adClasses[i]);
+    for (var j = 0; j < ads.length; j++) {
+      ads[j].style.display = 'none';
+    }
+  }
 }
 
-// 非表示にしたい要素のセレクターを指定して、hideElement関数を呼び出す
-// 例: クラス名が「ad」の要素を非表示にする
-hideElement('.ad');
-
-// 例: idが「banner」の要素を非表示にする
-hideElement('#container');
+window.addEventListener('load', hideAds);
